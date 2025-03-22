bff9c5081840ecfbcc0db8bd9e234a77045e590f
Minh Hoang FE <minhoang24fe.dev@gmail.com>
2025-03-22 17:46:21 +0700

    C1: Setup tailwind css

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div :class="{ 'dark': darkMode }">
+    <div>
+      <h1 class="text-red-500 dark:text-dim-500" >Twitter clone</h1> 
+    </div>
+  </div>
+</template>
+<script setup>
+  const darkMode = ref(true)
+</script>
